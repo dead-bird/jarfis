1acf5a0dab2158e3f3ccdf801173155073c16f7c
DaveP <dave@wearecrystal.uk>
2017-08-17 17:46:25 +0100

    scaffolded node project and started the basics

---
+++ app/__tests__/memestars.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const memestars = require('../index.js');
+
+describe('memestars', function () {
+  it('has a test', function () {
+    assert(false, 'memestars should have a test');
+  });
+});
