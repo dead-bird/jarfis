c37bf74100e4e864bef5c642a3177645972aad6a
DaveP <dave@wearecrystal.uk>
2017-08-18 17:29:52 +0100

    delete user message and made botname a const

--- app/rate.js
+++ app/rate.js
@@ -4,6 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.TOKEN;
 const regex = /!r(.*)([0-5])/g;
+const botName = 'memestar'
 
 var greetings = ['hey', 'hi', 'yo', 'sup', 'sound', 'safe'];
 
@@ -15,24 +16,30 @@ client.on('message', message => {
   var match = regex.exec(message.content);
 
   if (match) {
-    message.channel.send('', {
-        file: `resources/${match[2]}.png`
+    message.delete()
+     .then()
+     .catch(console.error);
+
+    message.channel.send(message.member.nickname ? message.member.nickname : message.author.username, {
+      file: `resources/${match[2]}.png`
     });
   }
   
-  if (message.content == 'bye memestar') {
+  if (message.content == `bye ${botName}`) {
     message.channel.send('see ya, wouldn\'t wanna meme ya');
   }
 
-  if (greetings.indexOf(message.content.split(' ')[0]) > -1 && message.content.substr(message.content.length - 8) == 'memestar') {
+  if (greetings.indexOf(message.content.split(' ')[0]) > -1 && message.content.split(' ')[1] == botName) {
     message.channel.send('hi bitches');
   }
 
-  if (message.content == 'clear'){
+  if (message.content == 'clear') {
+    message.delete()
+     .then()
+     .catch(console.error);
+
     message.channel.send('.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n+--------+\n+**CLEAR**+\n+--------+');
   }
-
-
 });
 
 client.login(token);
