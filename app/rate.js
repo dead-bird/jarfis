6bb659901c202769e26e07c087caa7103f3d9bb1
DaveP <dave@wearecrystal.uk>
2017-08-24 11:28:37 +0100

    Started working on bot.json implementation && other bs

--- app/rate.js
+++ app/rate.js
@@ -1,35 +1,56 @@
 require('dotenv').config({path: '../.env'});
 
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.TOKEN;
-const regex = /^!r(|\s*)([0-5])/g;
-const botName = 'Jarfis'
+const token = 'MzQ5MTkzMjUzNTYxNTY1MTg2.DH801w.fccyJIqNVSegZoX1HKSy7xdS0aM';
+const rate = /^!r(|\s*)([0-5])/g;
+
+var bot;
+
+try {
+  bot = JSON.parse(fs.readFileSync('app/data/bot.json'));
+} catch (err) {
+  name('Jarfis');
+}
 
 var greetings = ['hey', 'hi', 'yo', 'sup', 'sound', 'safe'];
+var nameChange = new RegExp('hey (' + bot.name + '), change your name to (.*)');
+
+function name(name) {
+  fs.writeFileSync('app/data/bot.json', JSON.stringify({name: name}));
+
+  bot = JSON.parse(fs.readFileSync('app/data/bot.json'));
+}
 
 client.on('ready', () => {
   console.log('ready to meme');
 });
 
 client.on('message', message => {
-  var match = regex.exec(message.content);
+  var rateMatch = rate.exec(message.content);
+  var nameMatch = nameChange.exec(message.content);
 
-  if (match) {
+  if (rateMatch) {
     message.delete()
      .then()
      .catch(console.error);
 
     message.channel.send(message.member.nickname ? message.member.nickname : message.author.username, {
-      file: `resources/${match[2]}.png`
+      file: `resources/${rateMatch[2]}.png`
     });
   }
   
-  if (message.content == `bye ${botName}`) {
+  if (message.content == `bye ${bot.name}`) {
     message.channel.send('see ya, wouldn\'t wanna meme ya');
   }
 
-  if (greetings.indexOf(message.content.split(' ')[0]) > -1 && message.content.split(' ')[1] == botName) {
+  if (nameMatch) {
+    name(nameMatch[2]);
+    message.channel.send('sure thing my dude');
+  }
+
+  if (greetings.indexOf(message.content.split(' ')[0]) > -1 && message.content.split(' ')[1] == bot.name) {
     message.channel.send('hi bitches');
   }
 
@@ -39,6 +60,23 @@ client.on('message', message => {
      .catch(console.error);
 
     message.channel.send('.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n+--------+\n+**CLEAR**+\n+--------+');
+  }
+
+  if (message.content == '!commands') {
+    message.channel.send(`be patient, ${message.member.nickname ? message.member.nickname : message.author.username}, I'm bloody working on it.`);
+  }
+
+  if (message.content == '!name') {
+    message.channel.send(`the name's ${bot.name}, don't wear it out`);
+  }
+
+  if (message.content == 'too pure for this world') {
+    message.channel.send(`thank you senpai`);
+  }
+
+  if (message.content == '!reset') {
+    name('Jarfis');
+    message.channel.send(`reverting to ${bot.name}. Don't fuck me up again I'm a soft boy`);
   }
 });
 
