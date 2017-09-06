289b70fd67820d311fb39ac25c350e25d7dc0c61
DaveP <dave@wearecrystal.uk>
2017-09-06 16:23:06 +0100

    Started flip embed
    need to make it less shite

--- app/commands.js
+++ app/commands.js
@@ -96,9 +96,17 @@ module.exports = {
   flip: {
     desc: 'Flip a coin.',
     args: '',
-    execute: (client, msg) => {
-      var res = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-      msg.channel.send(`it's ${res} motherfucker`);
+    execute: (client, msg) => { //should be more modular and less shit
+      var heads = 'https://i.gyazo.com/e380b49fc9e2b8b86571975f7df01d52.gif',
+          tails = 'https://i.gyazo.com/8697b5c1f85e43ec9580bc59727c5fcc.gif',
+          res   = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails',
+          embed = new Discord.RichEmbed()
+          .setColor((res === 'heads' ? '3232ff' : 'FFD700'))
+          .setTitle(`it's ${res} motherfucker`)
+          .setThumbnail((res === 'heads' ? heads : tails))
+          .addBlankField(true);
+
+      msg.channel.send({embed});
     }
   },
   rc: {
