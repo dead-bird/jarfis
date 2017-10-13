81c9a2d82c8d88ea71a17de5683360ed2cd9539d
Unknown <tobyjgray96@gmail.com>
2017-10-13 14:18:42 +0100

    Added auto response to keywords
    For now all just in a JSON file, next push will be adding functions to view and modify the triggers and responses

--- app/jarfis.js
+++ app/jarfis.js
@@ -7,16 +7,14 @@ const client = new Discord.Client({forceFetchUsers: true})
 const token = process.env.TOKEN;
 const prefix = '!'; //this is still hard coded in commands.js
 const insults = fs.readFileSync('app/data/insults.txt').toString().split('\n');
-const responses = getResponses();
+const responses = importResponses();
 
 client.on('ready', () => {
   console.log('ready to meme');
   if(process.env.ENV ===`Kanto`) {insultRand();}
-  console.log(responses);
 });
 
 client.on('message', msg => {
-  console.log(msg.member + ':' + msg.content);
   //loop through the commands module if msg starts with prefix
   if (msg.content.startsWith(prefix)) {
     args = msg.content.slice(prefix.length).split(' ');
@@ -24,8 +22,10 @@ client.on('message', msg => {
     if (args[0] in commands) {
       commands[args[0]].execute(client, msg, args);
     }
+  } else {
+    if (responses.hasOwnProperty(msg.content.toLowerCase()))
+      msg.channel.send(responses[msg.content.toLowerCase()]);
   }
-
 });
 
 function insultRand() {
@@ -46,11 +46,22 @@ function insultRand() {
 }
 
 function getBot(msg) {
-  return (msg.guild.member(client.user).nickname ? msg.guild.member(client.user).nickname : client.user.username);
+  try {
+    if ('guild' in msg && 'member' in msg.guild && 'user' in client && msg.guild.member(client.user).nickname) {
+      var bot = msg.guild.member(client.user).nickname;
+    } else {
+      var bot = client.user.username;
+    }
+  } catch (e) {
+    var bot = 'Jarfis';
+  }
+
+  return bot;
 }
 
-function getResponses() {
-  return JSON.parse(fs.readFileSync('app/data/responses.json', 'utf8'));
+function importResponses() {
+  let botName = getBot
+  return JSON.parse(fs.readFileSync('app/data/responses.json', 'utf8').replace(/{{bot}}/g, botName)); //just a one of var replacement can expand in future if want to go balls to the wall mental with it
 }
 
 client.login(token);
