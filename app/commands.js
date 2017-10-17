708f0140ab0e810242670152d7bed7f7d4458bd7
DaveP <dave@wearecrystal.uk>
2017-10-17 16:18:20 +0100

    Fixing eslint errors

--- app/commands.js
+++ app/commands.js
@@ -9,35 +9,42 @@ module.exports = {
   help: {
     desc: 'Lists all available commands.',
     args: '',
-    execute: (client, msg, args) => {
+    execute: (client, msg) => {
       msg.delete()
         .then()
         .catch(console.error);
 
-      var embed = new Discord.RichEmbed()
+      let embed = new Discord.RichEmbed()
         .setColor(3447003)
-        .setDescription('\:information_source\: here are my commands')
-        .setThumbnail(client.user.avatarURL) // bot's avatar
+        .setDescription(':information_source: here are my commands')
+        .setThumbnail(client.user.avatarURL) // Bot's avatar
         .addBlankField(true);
+
       for (var command in module.exports) {
-        embed.addField(`!${command} ${module.exports[command].args}`, module.exports[command].desc);
+        if (command) {
+          embed.addField(`!${command} ${module.exports[command].args}`, module.exports[command].desc);
+        }
       }
+
       msg.channel.send({embed});
     }
   },
   name: {
     desc: 'Returns the current name of the bot.',
     args: '',
-    execute: (client, msg, args) => {
+    execute: (client, msg) => {
+      let bot;
+
       try {
         if ('guild' in msg && 'member' in msg.guild && 'user' in client && msg.guild.member(client.user).nickname) {
-            var bot = msg.guild.member(client.user).nickname;
+          bot = msg.guild.member(client.user).nickname;
         } else {
-            var bot = client.user.username;
-          }
-      } catch(e) {
-        var bot = 'Jarfis';
+          bot = client.user.username;
+        }
+      } catch (e) {
+        bot = 'Jarfis';
       }
+
       msg.channel.send(`the name's ${bot}, don't wear it out`);
     }
   },
@@ -65,17 +72,16 @@ module.exports = {
     desc: 'Changes the name of the bot.',
     args: '<string: no spaces... for now>',
     execute: (client, msg, args) => {
-      var bot,
-          newName = '';
+      let newName = '';
 
-      for(i = 1 ; i < args.length ; i ++){
+      for (let i = 1; i < args.length; i++) {
         newName += args[i] + ' ';
       }
 
       msg.guild.member(client.user).setNickname(newName).then(function () {
-        bot = (msg.guild.member(client.user).nickname ? msg.guild.member(client.user).nickname : client.user.username);
+        let bot = (msg.guild.member(client.user).nickname ? msg.guild.member(client.user).nickname : client.user.username);
         msg.channel.send(`just call me ${bot}`);
-      }).catch(error => msg.reply(`can't do that my dude: ${error}`))
+      }).catch(error => msg.reply(`can't do that my dude: ${error}`));
     }
   },
   reset: {
@@ -84,25 +90,25 @@ module.exports = {
     execute: (client, msg) => {
       msg.guild.member(client.user).setNickname('Jarfis').then(function () {
         msg.channel.send(`reverting to Jarfis. Don't fuck me up again I'm a soft boy`);
-      }).catch(error => msg.reply(`can't do that my dude: ${error}`))
+      }).catch(error => msg.reply(`can't do that my dude: ${error}`));
     }
   },
   tweet: {
     desc: 'Pulls in a tweet.',
     args: '<string: URL of the tweet to pull in> <integer>',
-    execute: (client, msg, args) => {
-      var tw = new Twitter({
-        consumer_key: env.TWITTER_CONSUMER_KEY,
-        consumer_secret: env.TWITTER_CONSUMER_SECRET,
-        access_token_key: env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET
-      });
+    execute: (client, msg) => {
+      // let tw = new Twitter({
+      //   consumer_key: env.TWITTER_CONSUMER_KEY,
+      //   consumer_secret: env.TWITTER_CONSUMER_SECRET,
+      //   access_token_key: env.TWITTER_ACCESS_TOKEN_KEY,
+      //   access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET
+      // });
 
-      tw.get('favorites/list', function(error, tweets, response) {
-        // if(error) throw error;
-        console.log('wip');  // The favorites.
-        // console.log(response);  // Raw response object.
-      });
+      // tw.get('favorites/list', function (error, tweets, response) {
+      //   // If(error) throw error;
+      //   console.log('wip'); // The favorites.
+      //   // console.log(response);  // Raw response object.
+      // });
 
       msg.channel.send(`haha yes that was a tweet`);
     }
@@ -110,15 +116,15 @@ module.exports = {
   flip: {
     desc: 'Flip a coin.',
     args: '',
-    execute: (client, msg) => { //should be more modular and less shit
-      var heads = 'https://i.gyazo.com/e380b49fc9e2b8b86571975f7df01d52.gif',
-          tails = 'https://i.gyazo.com/8697b5c1f85e43ec9580bc59727c5fcc.gif',
-          res   = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails',
-          embed = new Discord.RichEmbed()
-          .setColor((res === 'heads' ? '3232ff' : 'FFD700'))
-          .setTitle(`it's ${res} motherfucker`)
-          .setThumbnail((res === 'heads' ? heads : tails))
-          .addBlankField(true);
+    execute: (client, msg) => { // Should be more modular and less shit
+      let heads = 'https://i.gyazo.com/e380b49fc9e2b8b86571975f7df01d52.gif';
+      let tails = 'https://i.gyazo.com/8697b5c1f85e43ec9580bc59727c5fcc.gif';
+      let res = (Math.floor(Math.random() * 2) === 0) ? 'heads' : 'tails';
+      let embed = new Discord.RichEmbed()
+        .setColor((res === 'heads' ? '3232ff' : 'FFD700'))
+        .setTitle(`it's ${res} motherfucker`)
+        .setThumbnail((res === 'heads' ? heads : tails))
+        .addBlankField(true);
 
       msg.channel.send({embed});
     }
@@ -127,21 +133,20 @@ module.exports = {
     desc: 'RaNCApS YOUR TeXt.',
     args: '<string>',
     execute: (client, msg, args) => {
-      var str = '',
-          a   = {},
-          i   = 0,
-          n,
-          r;
+      let str = '';
+      let i = 0;
 
       for (i; i < args.length; i++) {
-        if (i !== 0) { str = str += args[i] + ' '; }
+        if (i !== 0) {
+          str += args[i] + ' ';
+        }
       }
 
-      a = str.split(""),
-      n = a.length;
+      let a = str.split('');
+      let n = a.length;
 
-      for (var i = n - 1; i >= 0; i--) {
-        r = Math.floor(Math.random() * n) + 1;
+      for (i = n - 1; i >= 0; i--) {
+        let r = Math.floor(Math.random() * n) + 1;
         a[r] = (a[r] ? a[r].toUpperCase() : '');
       }
 
@@ -149,7 +154,7 @@ module.exports = {
         .then()
         .catch(console.error);
 
-      msg.channel.send(a.join(""));
+      msg.channel.send(a.join(''));
     }
   },
   clear: {
@@ -164,57 +169,60 @@ module.exports = {
     }
   },
   nut: {
-  	desc: 'Good Delicious Nut.',
-  	args: '',
-  	execute: (client, msg) => {
-  		msg.delete()
+    desc: 'Good Delicious Nut.',
+    args: '',
+    execute: (client, msg) => {
+      msg.delete()
         .then()
         .catch(console.error);
 
-    msg.channel.send('Let you fuck my face\nGood nutritious nut, on deck nigga')
-  	}
+      msg.channel.send('Let you fuck my face\nGood nutritious nut, on deck nigga');
+    }
   },
   think: {
     desc: 'Shows a random thinking emoji',
     args: '',
     execute: (client, msg) => {
-      var filePath = './app/resources/thinking/'
+      let filePath = './app/resources/thinking/';
 
-      fs.readdir(filePath, (err, data)=>{
+      fs.readdir(filePath, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          let r = Math.floor(Math.random() * data.length);
+          let selection = filePath + data[r];
 
-        var r = Math.floor(Math.random() * data.length),
-            selection = filePath + data[r]
+          msg.delete()
+            .then()
+            .catch(console.error);
 
-        msg.delete()
-          .then()
-          .catch(console.error);
-
-        msg.channel.send({
-          file: selection
-        });
-      })
+          msg.channel.send({
+            file: selection
+          });
+        }
+      });
     }
   },
   echo: {
     desc: 'Get Jarfis to parrot you.',
     args: '<string>',
     execute: (client, msg, args) => {
-      var str = '';
+      let str = '';
 
-      for(let i = 0 ; i < args.length ; i++){
-        if (i !== 0){
+      for (let i = 0; i < args.length; i++) {
+        if (i !== 0) {
           str += args[i] + ' ';
         }
       }
 
       msg.delete()
-          .then()
-          .catch(console.error);
+        .then()
+        .catch(console.error);
 
       msg.channel.send(str);
     }
   },
-  // join: {
+  // Join: {
   //   desc: 'Summons Jarfis to your current voice channel',
   //   args: '',
   //   execute: (client, msg) => {
@@ -249,26 +257,26 @@ module.exports = {
     desc: 'Add a trigger and response to the bot',
     args: '"<Trigger:string>", "<Response:string>"',
     execute: () => {
-      //gitignore the file so local boys not overwrote
-      //write trigger as key response as value to the JSON file
-      //echo command successfully added and repeat what was added in an embed
+      // Gitignore the file so local boys not overwrote
+      // write trigger as key response as value to the JSON file
+      // echo command successfully added and repeat what was added in an embed
     }
   },
   delResp: {
     desc: 'Delete a trigger and response from the bot',
-    args: '"<Trigger:string>"', //Just the trigger needed to delete from the JSON
+    args: '"<Trigger:string>"', // Just the trigger needed to delete from the JSON
     execute: () => {
-      //search for trigger and delete from file
-      //respond with the trigger and response deleted so canbe readded if mistake?
-      //allow to delete with number in list as well as trigger?
+      // Search for trigger and delete from file
+      // respond with the trigger and response deleted so canbe readded if mistake?
+      // allow to delete with number in list as well as trigger?
     }
   },
   showResp: {
     desc: 'List all the triggers and responses written to the bot',
     args: '',
     execute: () => {
-      //loop over file and list all triggers and responses in a discord embed
-      //maybe paginate responses a la the rythm queue
+      // Loop over file and list all triggers and responses in a discord embed
+      // maybe paginate responses a la the rythm queue
     }
   }
 };
