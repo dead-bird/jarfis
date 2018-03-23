a6b7d8b3cb373676f509d3e3b8e6470e36612f3f
Unknown <DylanTarry1451@hotmail.co.uk>
2018-03-23 13:01:11 +0000

    Added !insult command
    Dont @ me

--- app/commands.js
+++ app/commands.js
@@ -434,6 +434,20 @@ let self = module.exports = {
       msg.channel.send(meme);
       msg.delete().catch(console.error);
     }
+  },
+  insult: {
+    desc: '@ me',
+    args: '@user',
+    execute: (client, msg, args) => {
+      if (!args.length) return msg.reply(`Small **oof** my dude I need someone to insult`);
+      let user = args[0].match(/<@!\d*>/g);
+
+      if (!user) return msg.reply(`Small **oof** my dude *${args[0]}* isnt a user`);
+      let insults = fs.readFileSync(`${__dirname}/data/insults.txt`).toString().split('\n');
+      let selectedInsult = insults[Math.floor(Math.random() * insults.length)];
+
+      msg.channel.send(user + ' you ' + selectedInsult);
+    }
   }
 };
 
