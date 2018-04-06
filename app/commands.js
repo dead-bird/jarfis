3f0be49e861b070b5f5404f22a684d73561ad407
DaveP <dave@wearecrystal.uk>
2018-04-06 15:42:44 +0100

    Math.floor `!r` arguments

--- app/commands.js
+++ app/commands.js
@@ -42,16 +42,16 @@ let self = module.exports = {
   r: {
     desc: 'Rates a meme.',
     args: '<integer between 0 and 5>',
-    valid: (i) => {
-      return i >= 0 && i <= 5;
-    },
+    valid: i => i >= 0 && i <= 5,
     execute: (client, msg, args) => {
-      if (!self.r.valid(args[0])) return msg.reply('gimme dat fatty number between 0-5 ya\'dig'); // add to error handler
+      let rate = Math.floor(args[0]);
+
+      if (!self.r.valid(rate)) return msg.reply('gimme dat fatty number between 0-5 ya\'dig'); // add to error handler
 
       msg.delete().then().catch(console.error);
 
       msg.channel.send(msg.member.nickname || msg.author.username, {
-        file: `app/resources/responses/rate/${args[0]}.png`
+        file: `app/resources/responses/rate/${rate}.png`
       }).catch(err => core.err.dead(msg, err));
     }
   },
