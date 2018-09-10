61636612a4d38c9f6a8f6d0f36740606f33685f8
DaveP <dave@wearecrystal.uk>
2018-09-10 11:51:05 +0100

    bugs? *sips whiskey*,  haven't heard that name in years

--- app/commands.js
+++ app/commands.js
@@ -15,11 +15,11 @@ let self = module.exports = {
       let pf = core.server.get(client, msg.guild).prefix;
 
       let embed = new Discord.RichEmbed().setAuthor('command me daddy', client.user.avatarURL).setColor(3447003)
-      
+
       for (var cmd in module.exports) {
         if (cmd) embed.addField('\u200B', `**${pf}${cmd} ${module.exports[cmd].args}**\n${module.exports[cmd].desc}`);
       }
-      
+
       msg.delete().then().catch(console.error);
       msg.channel.send({embed});
     }
@@ -170,11 +170,11 @@ let self = module.exports = {
           user = core.user.get(client, id);
 
       if (user.banned) return msg.channel.send(`<@${id}> is already banned my dude`);
-      
+
       user.banned = true;
-      
+
       client.losers.set(id, user);
-      
+
       msg.channel.send(`<@${id}> is now banned`).catch(err => core.err.dead(msg, err));
     }
   },
@@ -186,11 +186,11 @@ let self = module.exports = {
           user = core.user.get(client, id);
 
       if (!user.banned) return msg.channel.send(`<@${id}> isn't even banned you ***idiot***`);
-      
+
       user.banned = false;
-      
+
       client.losers.set(id, user);
-      
+
       msg.channel.send(`<@${id}> is now unbanned`).catch(err => core.err.dead(msg, err));
     }
   },
@@ -201,7 +201,7 @@ let self = module.exports = {
     args: '"Trigger" "Response"',
     execute: (client, msg, args) => {
       if (args.length < 2) return core.err.args(msg);
-      
+
       let server = core.server.get(client, msg.guild);
       let str = '';
 
@@ -218,7 +218,7 @@ let self = module.exports = {
       }
 
       if (server.responses[trigger]) return msg.channel.send('be more original - trigger already exists').catch(err => core.err.dead(msg, err));
-      
+
       server.responses[trigger] = {
         response: response,
         author: msg.author.id
@@ -231,10 +231,10 @@ let self = module.exports = {
   },
   remove: {
     desc: 'Delete a trigger and response from the bot',
-    args: '"Trigger"', 
+    args: '"Trigger"',
     execute: (client, msg, args) => {
       if (!args.length) return core.err.args(msg, 1);
-      
+
       let server = core.server.get(client, msg.guild);
       let str = '';
 
@@ -273,7 +273,7 @@ let self = module.exports = {
         if (!guild.responses.hasOwnProperty(res)) break;
 
         let trigger = guild.responses[res], author  = '';
-        
+
         if (trigger.author) author = `\n*(added by ${client.users.get(trigger.author)})*` || ''; // fallback for old style responses without author
 
         fields.push({ name: '\u200B', value: `**${res}**\n${trigger.response || trigger}${author}` });
@@ -297,14 +297,14 @@ let self = module.exports = {
     args: '<string>(-raw)',
     execute: (client, msg, args) => {
       let clap = ':clap:';
-      
+
       if (args[0] === '-raw') {
         clap = '\\👏';
         args.splice(0, 1);
       }
-      
+
       let str = clap; // Prepend a clap
-      
+
       args.forEach(arg => { str += arg + clap });
 
       msg.delete().then().catch(console.error);
@@ -316,7 +316,7 @@ let self = module.exports = {
     args: '<string> A-Z and 0-9',
     execute: (client, msg, args) => {
       if (!args.length) return core.err.empty(msg);
-      
+
       let str = '';
 
       args.forEach(arg => { str += arg + ' ' });
@@ -337,7 +337,7 @@ let self = module.exports = {
       // Hard code one as an example, need to think about a more dynamic approach
       // For all the legit new cool mems on the block dawg
       if (args.length < 2) return core.err.args(msg);
-      
+
       let str = '';
 
       args.forEach(arg => { str += arg + ' ' });
@@ -447,7 +447,7 @@ let self = module.exports = {
     args: '@user',
     execute: (client, msg, args) => {
       if (!args.length) return msg.reply(`Small **oof** my dude I need someone to insult`);
-      let user = args[0].match(/<@!\d*>/g);
+      let user = args[0].match(/<@\!?\d*>/g);
 
       if (!user) return msg.reply(`Small **oof** my dude *${args[0]}* isnt a user`);
       let insults = fs.readFileSync(`${__dirname}/data/insults.txt`).toString().split('\n');
