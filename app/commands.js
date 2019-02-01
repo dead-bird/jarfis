860d3b7fb2e257297dfe47a27c4b58ed06e5686e
TobyG <toby@wearecrystal.uk>
2019-02-01 10:03:02 +0000

    !ss oh no i made it worse

--- app/commands.js
+++ app/commands.js
@@ -549,9 +549,19 @@ let self = (module.exports = {
 
       let str = `||${args[0]}||`.replace(/ /gi, '|| ||');
 
-      if (args[1]) {
-        if ((args[1] = 'raw')) {
-          str = `\`\`\`${str}\`\`\``;
+      for (let i = 1; i < args.length; i++) {
+        //Not forEach to skip the first arg??
+        arg = args[i];
+        console.log(arg);
+        switch (arg) {
+          case 'raw':
+            str = `\`\`\`${str}\`\`\``;
+            break;
+          case 'letter':
+            str = str.slice(2, -2).replace(/(.)/gi, '||$1||');
+            break;
+          default:
+            break;
         }
       }
 
