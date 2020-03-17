ac6f7cf5bc372ad8cce929398cd32682462e4fe5
Dylan <dylantarry1451@homtail.co.uk>
2020-03-17 11:26:04 +0000

    formatting

--- jarfis.js
+++ jarfis.js
@@ -1,9 +1,8 @@
-
 require('dotenv').config({ path: '.env' });
 
 const Discord = require('discord.js');
-const Client = new Discord.Client({ forceFetchUsers: true });
-const Message = require('./controllers/message');
+const client = new Discord.Client({ forceFetchUsers: true });
+const message = require('./controllers/message');
 const db = require('./controllers/db');
 const core = require('./controllers/core');
 const pkg = require('./package.json');
@@ -11,12 +10,12 @@ const jarfisModel = require('./models/jarfis');
 const serverModel = require('./models/servers');
 const ObjectId = require('mongodb').ObjectID;
 
-Client.login(process.env.TOKEN);
-Client.on('error', err => console.error(err));
-Client.on('warn', err => console.warn(err));
-Client.on('ready', async () => {
-    console.log(`${Client.user.tag} is online @ version ${pkg.version}`);
-    Client.user.setPresence({ activity: { name: `in ${process.env.LOC} @ ${pkg.version}` } });
+client.login(process.env.TOKEN);
+client.on('error', err => console.error(err));
+client.on('warn', err => console.warn(err));
+client.on('ready', async () => {
+    console.log(`${client.user.tag} is online @ version ${pkg.version}`);
+    client.user.setPresence({ activity: { name: `in ${process.env.LOC} @ ${pkg.version}` } });
 
     //Load database connection
     await db();
@@ -29,21 +28,21 @@ Client.on('ready', async () => {
     }
 
     //Check all current servers are in the db if not create one
-    Client.guilds.cache.map(async guild => {
+    client.guilds.cache.map(async guild => {
         let servers = await serverModel.read({ discordId: guild.id, name: guild.name });
         let id = guild.channels.cache.first().id;
 
         if (servers.length === 0) {
-            await serverModel.insert(core.server.default(Client, guild));
+            await serverModel.insert(core.server.default(client, guild));
             guild.channels.cache.first().send(`What up pimps! My default prefix is ! and your default channel is <#${id}>. Hit dat fatty !help to change shit`);
         }
     });
 });
-Client.on('message', async message => {
-    if (!message.author.bot && message.guild) {
-        const server = await serverModel.read({ discordId: message.guild.id });
-        if (server[0] && message.content.startsWith(server[0].settings.prefix)) {
-            Message(message, Client, server[0].settings.prefix)
+client.on('message', async msg => {
+    if (!msg.author.bot && msg.guild) {
+        const server = await serverModel.read({ discordId: msg.guild.id });
+        if (server[0] && msg.content.startsWith(server[0].settings.prefix)) {
+            message(msg, client, server[0].settings.prefix)
         }
     }
 });
