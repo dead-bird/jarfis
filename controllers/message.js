d9a4d1d0e3c19cd0e6373a5236b0cff39b58db20
Dylan <dylantarry1451@homtail.co.uk>
2020-03-17 14:35:11 +0000

    update regex

--- controllers/message.js
+++ controllers/message.js
@@ -1,7 +1,7 @@
 const Commands = require('../commands/_export');
 
 module.exports = (message, client, prefix) => {
-    const messageBreakdown = message.content.split(new RegExp(`^(?:${prefix}(\\S+))`));
+    const messageBreakdown = message.content.split(new RegExp(`^(?:\\${prefix}(\\S+))`));
     messageBreakdown.shift();
     const cmd = messageBreakdown[0];
     if (messageBreakdown[1]) {
